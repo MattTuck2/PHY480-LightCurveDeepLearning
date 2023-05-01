20cae0b7ec6c699beeec6ea30689f1c4da3f4fc8
Robert Fisher <robert.fisher@umassd.edu>
2023-04-30 21:59:42 -0400

    Cleaned up the merger script.

--- MantraMerge_Pandas.py
+++ MantraMerge_Pandas.py
@@ -15,12 +15,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os # use os library to specify machine-independent paths
 import pandas as pd
-import reader
+import reader_merge
 
 directory = os.path.join ("MANTRA", "data", "lightcurves")
 inputfile = os.path.join (directory, "transient_lightcurves.csv")
 
-light_curves = reader.read_light_curve_data(inputfile)
+light_curves = reader_merge.read_light_curve_data(inputfile)
 
 # Access a specific light curve data by object ID
 example_object_id = 1409030010044114444
@@ -36,18 +36,6 @@ classifications = pd.read_csv(inputfile)
 
 # rename columns
 classifications.columns = ['Classification', 'TransientID']
-
-# create a dictionary to search for Classification by TransientID
-#transient_dict = classifications.to_dict ('records')
-
-# example usage of the dictionary; classification should be a single 'SN?'
-#transient_id =  1409030010044114444
-
-# Search the dictionary for the matching Name entry
-#matching_records = [record for record in transient_dict if record['TransientID'] == transient_id]
-
-# Print the matching records
-#print(matching_records)
 
 #Merge the two dataframes on the 'TransientID' column
 merged_df = pd.merge(light_curves, classifications, left_on='ID', right_on='TransientID', how='left')
